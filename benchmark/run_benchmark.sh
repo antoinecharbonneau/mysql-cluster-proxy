fa34e6e9ddfffec1903a83da6fe5f228b128586d
Antoine Charbonneau <4383856-antoine.charbonneau@users.noreply.gitlab.com>
2022-12-23 19:32:32 -0500

    Added benchmark script

---
+++ benchmark/run_benchmark.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+sysbench oltp_read_write \
+    --table-size=1000000 \
+    --db-driver=mysql \
+    --mysql-db=sakila \
+    --mysql-user=root \
+    --mysql-password=${SQL_PASSWORD} \
+    prepare
+sysbench oltp_read_write \
+    --threads=6 \
+    --time=60 \
+    --max-requests=0 \
+    --db-driver=mysql \
+    --mysql-db=sakila \
+    --mysql-user=root \
+    --mysql-password=${SQL_PASSWORD} \
+    run
+sysbench oltp_read_write \
+    --db-driver=mysql \
+    --mysql-db=sakila \
+    --mysql-user=root \
+    --mysql-password=${SQL_PASSWORD} \
+    cleanup
